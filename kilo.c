28970c351e5f80a2de59cddd166388b7eaa20bf6
Alexei983 <alexei98377@gmail.com>
2025-02-18 16:56:43 +0000

    edit kilo.c

---
+++ kilo.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <errno.h>
+#include <ctype.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s){
+	perror(s);
+	exit(1);
+}
+
+void disableRawMode(){
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+void enableRawMode() {
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	atexit(disableRawMode);
+
+  	struct termios raw = orig_termios;
+	
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | ICANON | ISIG);
+
+  	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattor");
+}
+
+char editorReadKey() {
+  	int nread;
+  	char c;
+  	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    		if (nread == -1 && errno != EAGAIN) die("read");
+  	}
+  	return c;
+}
+
+/*** input ***/
+
+void editorProcessKeypress() {
+	char c = editorReadKey();
+  	switch (c) {
+    		case CTRL_KEY('q'):
+      			exit(0);
+     			break;
+  	}
+}
+
+/*** init ***/
+
+int main() {
+  	enableRawMode();
+ 	
+	while (1) {
+		editorProcessKeypress();
+	}
+
+  	return 0;
+}
+
