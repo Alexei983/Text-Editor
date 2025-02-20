dd438ffccd1ac0af3c3ff812402c34fd7908a8d2
Alexei983 <alexei98377@gmail.com>
2025-02-20 13:36:51 +0000

    edit kilo.c

--- kilo.c
+++ kilo.c
@@ -9,22 +9,35 @@
 
 struct termios orig_termios;
 
+/*** includes ***/
+
+/*** data ***/
+
+struct editorConfig {
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
+
 /*** terminal ***/
 
 void die(const char *s){
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
 
 void disableRawMode(){
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) die("tcsetattr");
 }
 
 void enableRawMode() {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
-  	struct termios raw = orig_termios;
+  	struct termios raw = E.orig_termios;
 	
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   	raw.c_oflag &= ~(OPOST);
@@ -43,13 +56,33 @@ char editorReadKey() {
   	return c;
 }
 
+/*** output ***/
+
+void editorDrawRows() {
+	int y;
+	for (y = 0; y < 24; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /*** input ***/
 
 void editorProcessKeypress() {
 	char c = editorReadKey();
   	switch (c) {
     		case CTRL_KEY('q'):
-      			exit(0);
+      			write(STDOUT_FILENO, "\x1b[2J", 4);
+      			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0);
      			break;
   	}
 }
@@ -60,6 +93,7 @@ int main() {
   	enableRawMode();
  	
 	while (1) {
+		editorRefreshScreen();
 		editorProcessKeypress();
 	}
 
